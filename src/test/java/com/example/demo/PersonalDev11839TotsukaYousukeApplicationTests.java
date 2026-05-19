b3af55189e06fa9efa1332d252ae23d5ba1fc5c5
student <student@FJ-1025>
2026-05-19 11:38:31 +0900

    コミット

---
+++ src/test/java/com/example/demo/PersonalDev11839TotsukaYousukeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PersonalDev11839TotsukaYousukeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
